df0ee538d79e5960df376fdfba4ca435240e89e8
CleanPegasus <arunk609@gmail.com>
2020-03-27 13:01:41 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+import requests
+import logging
+import random
+
+
